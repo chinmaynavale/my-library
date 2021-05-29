85272b811a48c8eaf610a239171df8e6504b33b5
Chinmay Navale <chinmaynavale9300@gmail.com>
2021-05-29 19:35:26 +0530

    Creating Authors routes and views

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
@@ -25,5 +27,6 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to mongoose'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
