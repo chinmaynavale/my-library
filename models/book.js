c8676c8a07a0a1936cee193b5fcdfdd2aa8c320a
Chinmay Navale <chinmaynavale9300@gmail.com>
2021-06-02 15:47:31 +0530

    setting up book routes

--- models/book.js
+++ models/book.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const path = require('path');
 
 const coverImageBasePath = 'uploads/bookCovers';
 const bookSchema = new mongoose.Schema({
@@ -26,11 +27,17 @@ const bookSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  authorId: {
+  author: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
     ref: 'Author',
   },
+});
+
+bookSchema.virtual('coverImagePath').get(function () {
+  if (this.coverImageName != null) {
+    return path.join('/', coverImageBasePath, this.coverImageName);
+  }
 });
 
 module.exports = mongoose.model('Book', bookSchema);
